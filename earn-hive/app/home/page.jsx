bd6b9e743757ba7eb8e5230f1c831803596b0a82
chiscookeke11 <chiscookeke11@gmail.com>
2024-12-06 23:18:53 -1100

    footer

--- earn-hive/app/home/page.jsx
+++ earn-hive/app/home/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from "../../components/navbar/Navbar"
+import Footer from "../../components/footer/Footer"
 
 const page = () => {
   return (
     <div>
         <Navbar/>
         home
+        <Footer/>
     </div>
   )
 }
--- earn-hive/components/navbar/Navbar.jsx
+++ earn-hive/components/navbar/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
 
 
 
+  
   const handleLogout = () => {
     console.log("Logging out...");
    
